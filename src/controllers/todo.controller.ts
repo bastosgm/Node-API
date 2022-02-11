d71900f74790aefd2e123a99b37c20af6c69c19c
Gustavo Bastos <bastosgm@outlook.com>
2022-02-11 15:19:31 -0300

    adding todo.controller's methods (3/4)

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -17,8 +17,35 @@ export const add = async (req: Request, res: Response) => {
     res.json({ Error: 'Não foi possível adicionar nova task'})
   }
 }
-export const update = async () => {
+export const update = async (req: Request, res: Response) => {
+  const { id } = req.params
+  let task = await Todo.findByPk(id)
+  if(task) {
+     
+    if(req.body.title) {
+      task.title = req.body.title
+    }
 
+    if(req.body.done) {
+      //true,1 = false,0 opcional em apis
+      //reduz a lowerCase para facilitar o switch
+      //caso nao seja nenhuma das options, o valor atual de mantém
+      switch(req.body.done.toLowerCase()) { 
+      case 'true':
+      case '1':
+        task.done = true
+        break
+      case 'false':
+      case '0':
+        task.done = false
+        break
+      }
+    }
+     await task.save()
+     res.json({task})
+  } else {
+    res.json({Error: 'Item not found :('})
+  }
 }
 export const remove = async () => {
 
